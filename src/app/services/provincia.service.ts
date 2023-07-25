928046000e6e0b09b45e2a399e40268d27200223
Nahuel Moyano <naku.24moyano@gmail.com>
2023-07-25 11:34:43 -0300

    servicio manera local

--- src/app/services/provincia.service.ts
+++ src/app/services/provincia.service.ts
@@ -16,21 +16,44 @@ export class ProvinciaService {
   }
 
   addProvince(provincia: Provincia): Observable<Provincia> {
-    return this.http.post<Provincia>(`${this.API_URL}/data`, provincia);
+    return this.http.post<Provincia>(`${this.API_URL}`, provincia);
   }
 
   updateProvince(provincia: Provincia): Observable<Provincia> {
     return this.http.put<Provincia>(
-      this.API_URL + '/data' + '/' + provincia.ID,
+      this.API_URL + '/' + provincia.ID,
       provincia
     );
   }
 
   getProvinciaById(ID: number): Observable<Provincia> {
-    return this.http.get<Provincia>(`${this.API_URL}/data/${ID}`);
+    return this.http.get<Provincia>(`${this.API_URL}/${ID}`);
   }
 
   delete(provincia: Provincia): Observable<Provincia> {
     return this.http.delete<Provincia>(`${this.API_URL}/${provincia.ID}`);
+  }
+
+  // ------------------- servicios para conectarse de manera local
+  provinciaList: Provincia[] = [
+    { CP: '1', ID: 1, CIUDADID: 'CORDOBA', PROVINCIA: 'CORDOBITA' },
+    { CP: '2', ID: 10, CIUDADID: 'BS.AS', PROVINCIA: 'BS.AS' },
+    { CP: '3', ID: 100, CIUDADID: 'BARCELONA', PROVINCIA: 'MADRID' },
+    { CP: '4', ID: 1000, CIUDADID: 'PAMPA', PROVINCIA: 'MOSCU' },
+  ];
+
+  //devuelve copia del array
+  getProvinceStatic() {
+    return this.provinciaList.slice();
+  }
+
+  //para agregar uno o mas elementos al inicio del array
+  addProvinceStatic(p: Provincia) {
+    this.provinciaList.unshift(p);
+  }
+
+  //eliminar provincia del array
+  deletePRovinceStatic(index: number) {
+    this.provinciaList.splice(index, 1);
   }
 }
